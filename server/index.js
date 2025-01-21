9471d1282336789d35458e59479cf5e366e220d4
bhaktikulk <150662839+bhaktikulk@users.noreply.github.com>
2025-01-21 16:23:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,116 +1,3 @@
-// const express=require("express")
-// const mongoose=require('mongoose')
-// const cors=require("cors")
-// const EmployeeModel=require('./models/Employee')
-
-// const app=express()
-// app.use(express.json())
-// app.use(cors())
-
-// mongoose
-//   .connect("mongodb+srv://<Bhakti12>:<12345678a>@cluster.hozl3.mongodb.?retryWrites=true&w=majority&appName=Cluster", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-// //mongoose.connect("mongodb+srv://<Bhakti12>:<12345678a>@cluster.hozl3.mongodb.?retryWrites=true&w=majority&appName=Cluster");
-
-// app.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-  
-//   EmployeeModel.findOne({ email: email })
-//     .then(user => {
-//       if (user) {
-//         if (user.password === password) {
-//           res.json({ message: "Success", user }); // Send success message
-//         } else {
-//           res.status(401).json({ message: "The password you have entered is incorrect" }); // Password incorrect
-//         }
-//       } else {
-//         res.status(404).json({ message: "No record existed" }); // User not found
-//       }
-//     })
-//     .catch(err => res.status(500).json(err)); // Server error
-// });
-
-
-// app.post('/', (req,res) => {
-//     EmployeeModel.create(req.body)
-//     .then(employees=> res.json(employees))
-//     .catch(err=>res.json(err))
-// })
-
-// app.listen(3001, () => {
-//     console.log("server is running")
-// })
-
-// const express=require("express");
-// const mongoose=require('mongoose');
-// const cors=require("cors");
-// const EmployeeModel=require('./models/Employee');
-// //const fileUpload=require("express-fileupload");
-// //const cloudinary=require('cloudinary').v2;
-
-// // cloudinary.config({ 
-// //   cloud_name: 'dj2juzuu5', 
-// //   api_key: '465124593463732', 
-// //   api_secret: 'COr_Ct0q5d0OY4fGAmrXW3Jxl1o'
-// // });
-
-// const app=express()
-// app.use(cors())
-// app.use(express.json())
-// //app.use(fileUpload())
-// // app.use(cors(
-// //   {
-// //     origin:["https://stack-client-bay.vercel.app"],
-// //     methods:["POST","GET"],
-// //     credentials:true
-// //   }
-// //  ));
-
-// //const fileUpload=require("express-fileupload");
-
-
-// mongoose
-//   .connect("mongodb+srv://Bhakti12:12345678a@cluster.hozl3.mongodb.net/employee?retryWrites=true&w=majority", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-// //mongoose.connect("mongodb+srv://<Bhakti12>:<12345678a>@cluster.hozl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster");
-
-// // app.use(fileUpload({
-// //      useTempFiles:True
-// // }))
-
-// app.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-  
-//   EmployeeModel.findOne({ email: email })
-//     .then(user => {
-//       if (user) {
-//         if (user.password === password) {
-//           res.json({ message: "Success", user }); // Send success message
-//         } else {
-//           res.status(401).json({ message: "The password you have entered is incorrect" }); // Password incorrect
-//         }
-//       } else {
-//         res.status(404).json({ message: "No record existed" }); // User not found
-//       }
-//     })
-//     .catch(err => res.status(500).json(err)); // Server error
-// });
-
-
-// app.post('/', (req,res) => {
-//     EmployeeModel.create(req.body)
-//     .then(employees=> res.json(employees))
-//     .catch(err=>res.json(err))
-// })
-
-// app.listen(3001, () => {
-//     console.log("server is running")
-// })
-
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -139,6 +26,10 @@ mongoose
   )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.get("/", (res, req) => {
+   res.json("Hello");
+}
 
 // Schema and Model
 const EmployeeSchema = new mongoose.Schema({
